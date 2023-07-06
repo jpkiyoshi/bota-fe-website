1ad910a4a021a5073ebc8bab78746816fb130500
João P. Kiyoshi <kiyoshi33@gmail.com>
2023-07-05 21:47:29 -0300

    switch to netlify edge function

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: 'server',
-  adapter: netlify()
+	integrations: [react()],
+	output: 'server',
+	adapter: netlify(),
 });
