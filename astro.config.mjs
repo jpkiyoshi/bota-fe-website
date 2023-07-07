46fa06d05740f530695cef5757421282c7fbc540
João P. Kiyoshi <kiyoshi33@gmail.com>
2023-07-07 15:06:21 -0300

    switch from react to preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
 import netlify from '@astrojs/netlify/functions';
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react()],
+	integrations: [preact({ compat: true })],
 	output: 'server',
 	adapter: netlify(),
 });
